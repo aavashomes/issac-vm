fb3a44931c5e1f92dbf9ff45b351e99fa7a15aac
applewithcoffee <63943139+applewithcoffee@users.noreply.github.com>
2020-04-19 19:19:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDJEaT4VliwgO3tMAfrwktXD6TQc9fN60w",
+    authDomain: "aavashomes.firebaseapp.com",
+    databaseURL: "https://aavashomes.firebaseio.com",
+    projectId: "aavashomes",
+    storageBucket: "aavashomes.appspot.com",
+    messagingSenderId: "416827212266",
+    appId: "1:416827212266:web:1148e3a34a848b5d9ab293",
+    measurementId: "G-Q03PT4Z9B4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
