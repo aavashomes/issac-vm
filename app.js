cbd66a8bca2ea5e3279b034b5ca92a966d7f8200
mandiladitya <mandil.aditya@gmail.com>
2020-05-27 10:56:08 +0530

    Mark 1

---
+++ app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCOVRvLK4SdQBHw2BNAmPXF45Nhck8ddaY",
+    authDomain: "issac-8543d.firebaseapp.com",
+    databaseURL: "https://issac-8543d.firebaseio.com",
+    projectId: "issac-8543d",
+    storageBucket: "issac-8543d.appspot.com",
+    messagingSenderId: "314588981800",
+    appId: "1:314588981800:web:71aa8a1f04f88cd94e231d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
